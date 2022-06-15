866e8ffdf08f1f424541be3eea894d13e57c3997
Jeff <farlandjeff@gmail.com>
2022-06-15 15:59:31 -0400

    Add return cases to function

--- rps.js
+++ rps.js
@@ -2,4 +2,18 @@
 function computerPlay() {
     // Random number between 0 to 2
     let num = Math.floor(Math.random() * 3);
+
+    // Return Rock, Paper, or Scissors depending on which number
+    switch(num) {
+        case 0:
+            return 'Rock';
+
+        case 1:
+            return 'Paper';
+        
+        case 2:
+            return 'Scissors';
+    }
 }
+
+// console.log(computerPlay());
